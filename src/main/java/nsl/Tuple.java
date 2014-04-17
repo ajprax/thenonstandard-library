5bf1a5c388ca1fb1976ed14c577507139e727c05
Aaron Feldstein <afeldstein@wibidata.com>
2014-04-16 17:23:43 -0700

    Adds Tuple classes

---
+++ src/main/java/nsl/Tuple.java
@@ -0,0 +1,494 @@
+package nsl;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+
+/**
+ * Contains implementations of generic tuple classes to provide toString, hashCode, and equals.
+ *
+ * To use, extend the tuple class of the appropriate size and add getter methods.
+ */
+public class Tuple {
+
+  public static class Tuple0 {
+    private static final String mToString = "()";
+    @Override
+    public String toString() {
+      return mToString;
+    }
+    @Override
+    public int hashCode() {
+      return 0;
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      return obj instanceof Tuple0;
+    }
+  }
+
+  public static class Tuple1<FIRST> {
+    private static final String FORMAT = "(%s)";
+    protected final FIRST mFirst;
+    protected Tuple1(
+        final FIRST first
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+    }
+    @Override
+    public String toString() {
+      return String.format(FORMAT, mFirst);
+    }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple1)) {
+        return false;
+      }
+      final Tuple1 that = (Tuple1) obj;
+      return Objects.equal(this.mFirst, that.mFirst);
+    }
+  }
+
+  public static class Tuple2<FIRST, SECOND> {
+    private static final String FORMAT = "(%s, %s)";
+    protected final FIRST mFirst;
+    protected final SECOND mSecond;
+    protected Tuple2(
+        final FIRST first,
+        final SECOND second
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+      mSecond = Preconditions.checkNotNull(second);
+    }
+    @Override
+    public String toString() {
+      return String.format(FORMAT, mFirst, mSecond);
+    }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst, mSecond);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple2)) {
+        return false;
+      }
+      final Tuple2 that = (Tuple2) obj;
+      return Objects.equal(this.mFirst, that.mFirst)
+          && Objects.equal(this.mSecond, that.mSecond);
+    }
+  }
+
+  public static class Tuple3<FIRST, SECOND, THIRD> {
+    private static final String FORMAT = "(%s, %s, %s)";
+    protected final FIRST mFirst;
+    protected final SECOND mSecond;
+    protected final THIRD mThird;
+    protected Tuple3(
+        final FIRST first,
+        final SECOND second,
+        final THIRD third
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+      mSecond = Preconditions.checkNotNull(second);
+      mThird = Preconditions.checkNotNull(third);
+    }
+    @Override
+    public String toString() {
+      return String.format(FORMAT, mFirst, mSecond, mThird);
+    }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst, mSecond, mThird);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple3)) {
+        return false;
+      }
+      final Tuple3 that = (Tuple3) obj;
+      return Objects.equal(this.mFirst, that.mFirst)
+          && Objects.equal(this.mSecond, that.mSecond)
+          && Objects.equal(this.mThird, that.mThird);
+    }
+  }
+
+  public static class Tuple4<FIRST, SECOND, THIRD, FOURTH> {
+    private static final String FORMAT = "(%s, %s, %s, %s)";
+    protected final FIRST mFirst;
+    protected final SECOND mSecond;
+    protected final THIRD mThird;
+    protected final FOURTH mFourth;
+    protected Tuple4(
+        final FIRST first,
+        final SECOND second,
+        final THIRD third,
+        final FOURTH fourth
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+      mSecond = Preconditions.checkNotNull(second);
+      mThird = Preconditions.checkNotNull(third);
+      mFourth = Preconditions.checkNotNull(fourth);
+    }
+    @Override
+    public String toString() {
+      return String.format(FORMAT, mFirst, mSecond, mThird, mFourth);
+    }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst, mSecond, mThird, mFourth);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple4)) {
+        return false;
+      }
+      final Tuple4 that = (Tuple4) obj;
+      return Objects.equal(this.mFirst, that.mFirst)
+          && Objects.equal(this.mSecond, that.mSecond)
+          && Objects.equal(this.mThird, that.mThird)
+          && Objects.equal(this.mFourth, that.mFourth);
+    }
+  }
+
+  public static class Tuple5<FIRST, SECOND, THIRD, FOURTH, FIFTH> {
+    private static final String FORMAT = "(%s, %s, %s, %s, %s)";
+    protected final FIRST mFirst;
+    protected final SECOND mSecond;
+    protected final THIRD mThird;
+    protected final FOURTH mFourth;
+    protected final FIFTH mFifth;
+    protected Tuple5(
+        final FIRST first,
+        final SECOND second,
+        final THIRD third,
+        final FOURTH fourth,
+        final FIFTH fifth
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+      mSecond = Preconditions.checkNotNull(second);
+      mThird = Preconditions.checkNotNull(third);
+      mFourth = Preconditions.checkNotNull(fourth);
+      mFifth = Preconditions.checkNotNull(fifth);
+    }
+    @Override
+    public String toString() {
+      return String.format(FORMAT, mFirst, mSecond, mThird, mFourth, mFifth);
+    }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst, mSecond, mThird, mFourth, mFifth);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple5)) {
+        return false;
+      }
+      final Tuple5 that = (Tuple5) obj;
+      return Objects.equal(this.mFirst, that.mFirst)
+          && Objects.equal(this.mSecond, that.mSecond)
+          && Objects.equal(this.mThird, that.mThird)
+          && Objects.equal(this.mFourth, that.mFourth)
+          && Objects.equal(this.mFifth, that.mFifth);
+    }
+  }
+
+  public static class Tuple6<FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH> {
+    private static final String FORMAT = "(%s, %s, %s, %s, %s, %s)";
+    protected final FIRST mFirst;
+    protected final SECOND mSecond;
+    protected final THIRD mThird;
+    protected final FOURTH mFourth;
+    protected final FIFTH mFifth;
+    protected final SIXTH mSixth;
+    protected Tuple6(
+        final FIRST first,
+        final SECOND second,
+        final THIRD third,
+        final FOURTH fourth,
+        final FIFTH fifth,
+        final SIXTH sixth
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+      mSecond = Preconditions.checkNotNull(second);
+      mThird = Preconditions.checkNotNull(third);
+      mFourth = Preconditions.checkNotNull(fourth);
+      mFifth = Preconditions.checkNotNull(fifth);
+      mSixth = Preconditions.checkNotNull(sixth);
+    }
+    @Override
+    public String toString() {
+      return String.format(FORMAT, mFirst, mSecond, mThird, mFourth, mFifth, mSixth);
+    }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst, mSecond, mThird, mFourth, mFifth, mSixth);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple6)) {
+        return false;
+      }
+      final Tuple6 that = (Tuple6) obj;
+      return Objects.equal(this.mFirst, that.mFirst)
+          && Objects.equal(this.mSecond, that.mSecond)
+          && Objects.equal(this.mThird, that.mThird)
+          && Objects.equal(this.mFourth, that.mFourth)
+          && Objects.equal(this.mFifth, that.mFifth)
+          && Objects.equal(this.mSixth, that.mSixth);
+    }
+  }
+
+  public static class Tuple7<FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH> {
+    private static final String FORMAT = "(%s, %s, %s, %s, %s, %s, %s)";
+    protected final FIRST mFirst;
+    protected final SECOND mSecond;
+    protected final THIRD mThird;
+    protected final FOURTH mFourth;
+    protected final FIFTH mFifth;
+    protected final SIXTH mSixth;
+    protected final SEVENTH mSeventh;
+    protected Tuple7(
+        final FIRST first,
+        final SECOND second,
+        final THIRD third,
+        final FOURTH fourth,
+        final FIFTH fifth,
+        final SIXTH sixth,
+        final SEVENTH seventh
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+      mSecond = Preconditions.checkNotNull(second);
+      mThird = Preconditions.checkNotNull(third);
+      mFourth = Preconditions.checkNotNull(fourth);
+      mFifth = Preconditions.checkNotNull(fifth);
+      mSixth = Preconditions.checkNotNull(sixth);
+      mSeventh = Preconditions.checkNotNull(seventh);
+    }
+    @Override
+    public String toString() {
+      return String.format(FORMAT, mFirst, mSecond, mThird, mFourth, mFifth, mSixth, mSeventh);
+    }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst, mSecond, mThird, mFourth, mFifth, mSixth, mSeventh);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple7)) {
+        return false;
+      }
+      final Tuple7 that = (Tuple7) obj;
+      return Objects.equal(this.mFirst, that.mFirst)
+          && Objects.equal(this.mSecond, that.mSecond)
+          && Objects.equal(this.mThird, that.mThird)
+          && Objects.equal(this.mFourth, that.mFourth)
+          && Objects.equal(this.mFifth, that.mFifth)
+          && Objects.equal(this.mSixth, that.mSixth)
+          && Objects.equal(this.mSeventh, that.mSeventh);
+    }
+  }
+
+  public static class Tuple8<FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, EIGHTH> {
+    private static final String FORMAT = "(%s, %s, %s, %s, %s, %s, %s, %s)";
+    protected final FIRST mFirst;
+    protected final SECOND mSecond;
+    protected final THIRD mThird;
+    protected final FOURTH mFourth;
+    protected final FIFTH mFifth;
+    protected final SIXTH mSixth;
+    protected final SEVENTH mSeventh;
+    protected final EIGHTH mEighth;
+    protected Tuple8(
+        final FIRST first,
+        final SECOND second,
+        final THIRD third,
+        final FOURTH fourth,
+        final FIFTH fifth,
+        final SIXTH sixth,
+        final SEVENTH seventh,
+        final EIGHTH eighth
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+      mSecond = Preconditions.checkNotNull(second);
+      mThird = Preconditions.checkNotNull(third);
+      mFourth = Preconditions.checkNotNull(fourth);
+      mFifth = Preconditions.checkNotNull(fifth);
+      mSixth = Preconditions.checkNotNull(sixth);
+      mSeventh = Preconditions.checkNotNull(seventh);
+      mEighth = Preconditions.checkNotNull(eighth);
+    }
+    @Override
+    public String toString() {
+      return String.format(FORMAT, mFirst, mSecond, mThird, mFourth, mFifth, mSixth, mSeventh,
+          mEighth);
+    }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst, mSecond, mThird, mFourth, mFifth, mSixth, mSeventh, mEighth);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple8)) {
+        return false;
+      }
+      final Tuple8 that = (Tuple8) obj;
+      return Objects.equal(this.mFirst, that.mFirst)
+          && Objects.equal(this.mSecond, that.mSecond)
+          && Objects.equal(this.mThird, that.mThird)
+          && Objects.equal(this.mFourth, that.mFourth)
+          && Objects.equal(this.mFifth, that.mFifth)
+          && Objects.equal(this.mSixth, that.mSixth)
+          && Objects.equal(this.mSeventh, that.mSeventh)
+          && Objects.equal(this.mEighth, that.mEighth);
+    }
+  }
+
+  public static class Tuple9<FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, EIGHTH, NINTH> {
+    private static final String FORMAT = "(%s, %s, %s, %s, %s, %s, %s, %s, %s)";
+    protected final FIRST mFirst;
+    protected final SECOND mSecond;
+    protected final THIRD mThird;
+    protected final FOURTH mFourth;
+    protected final FIFTH mFifth;
+    protected final SIXTH mSixth;
+    protected final SEVENTH mSeventh;
+    protected final EIGHTH mEighth;
+    protected final NINTH mNinth;
+    protected Tuple9(
+        final FIRST first,
+        final SECOND second,
+        final THIRD third,
+        final FOURTH fourth,
+        final FIFTH fifth,
+        final SIXTH sixth,
+        final SEVENTH seventh,
+        final EIGHTH eighth,
+        final NINTH ninth
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+      mSecond = Preconditions.checkNotNull(second);
+      mThird = Preconditions.checkNotNull(third);
+      mFourth = Preconditions.checkNotNull(fourth);
+      mFifth = Preconditions.checkNotNull(fifth);
+      mSixth = Preconditions.checkNotNull(sixth);
+      mSeventh = Preconditions.checkNotNull(seventh);
+      mEighth = Preconditions.checkNotNull(eighth);
+      mNinth = Preconditions.checkNotNull(ninth);
+    }
+    @Override
+    public String toString() { return String.format(FORMAT, mFirst, mSecond, mThird, mFourth,
+        mFifth, mSixth, mSeventh, mEighth, mNinth); }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst, mSecond, mThird, mFourth, mFifth, mSixth, mSeventh, mEighth,
+          mNinth);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple9)) {
+        return false;
+      }
+      final Tuple9 that = (Tuple9) obj;
+      return Objects.equal(this.mFirst, that.mFirst)
+          && Objects.equal(this.mSecond, that.mSecond)
+          && Objects.equal(this.mThird, that.mThird)
+          && Objects.equal(this.mFourth, that.mFourth)
+          && Objects.equal(this.mFifth, that.mFifth)
+          && Objects.equal(this.mSixth, that.mSixth)
+          && Objects.equal(this.mSeventh, that.mSeventh)
+          && Objects.equal(this.mEighth, that.mEighth)
+          && Objects.equal(this.mNinth, that.mNinth);
+    }
+  }
+
+  public static class Tuple10<FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, EIGHTH, NINTH,
+      TENTH> {
+    private static final String FORMAT = "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)";
+    protected final FIRST mFirst;
+    protected final SECOND mSecond;
+    protected final THIRD mThird;
+    protected final FOURTH mFourth;
+    protected final FIFTH mFifth;
+    protected final SIXTH mSixth;
+    protected final SEVENTH mSeventh;
+    protected final EIGHTH mEighth;
+    protected final NINTH mNinth;
+    protected final TENTH mTenth;
+    protected Tuple10(
+        final FIRST first,
+        final SECOND second,
+        final THIRD third,
+        final FOURTH fourth,
+        final FIFTH fifth,
+        final SIXTH sixth,
+        final SEVENTH seventh,
+        final EIGHTH eighth,
+        final NINTH ninth,
+        final TENTH tenth
+    ) {
+      mFirst = Preconditions.checkNotNull(first);
+      mSecond = Preconditions.checkNotNull(second);
+      mThird = Preconditions.checkNotNull(third);
+      mFourth = Preconditions.checkNotNull(fourth);
+      mFifth = Preconditions.checkNotNull(fifth);
+      mSixth = Preconditions.checkNotNull(sixth);
+      mSeventh = Preconditions.checkNotNull(seventh);
+      mEighth = Preconditions.checkNotNull(eighth);
+      mNinth = Preconditions.checkNotNull(ninth);
+      mTenth = Preconditions.checkNotNull(tenth);
+    }
+    @Override
+    public String toString() {
+      return String.format(FORMAT, mFirst, mSecond, mThird, mFourth, mFifth, mSixth, mSeventh,
+          mEighth, mNinth, mTenth);
+    }
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(mFirst, mSecond, mThird, mFourth, mFifth, mSixth, mSeventh, mEighth,
+          mNinth, mTenth);
+    }
+    @Override
+    public boolean equals(
+        final Object obj
+    ) {
+      if (null == obj || !(obj instanceof Tuple10)) {
+        return false;
+      }
+      final Tuple10 that = (Tuple10) obj;
+      return Objects.equal(this.mFirst, that.mFirst)
+          && Objects.equal(this.mSecond, that.mSecond)
+          && Objects.equal(this.mThird, that.mThird)
+          && Objects.equal(this.mFourth, that.mFourth)
+          && Objects.equal(this.mFifth, that.mFifth)
+          && Objects.equal(this.mSixth, that.mSixth)
+          && Objects.equal(this.mSeventh, that.mSeventh)
+          && Objects.equal(this.mEighth, that.mEighth)
+          && Objects.equal(this.mNinth, that.mNinth)
+          && Objects.equal(this.mTenth, that.mTenth);
+    }
+  }
+}
---
+++ src/test/java/nsl/tuple/TestTuple.java
@@ -0,0 +1,63 @@
+package nsl.tuple;
+
+import junit.framework.Assert;
+import nsl.Tuple.Tuple2;
+import org.junit.Test;
+
+public class TestTuple {
+
+  private static final class MyPair extends Tuple2<String, Integer> {
+    private static final String FORMAT = "MyPair: %s";
+    private final String mToString;
+    public MyPair(final String s, final Integer integer) {
+      super(s, integer);
+      mToString = String.format(FORMAT, super.toString());
+    }
+    public String getName() {
+      return mFirst;
+    }
+    public Integer getId() {
+      return mSecond;
+    }
+    @Override
+    public String toString() {
+      return mToString;
+    }
+  }
+
+  @Test
+  public void testMyPair() {
+    final MyPair m = new MyPair("name", 1);
+    Assert.assertEquals("MyPair: (name, 1)", m.toString());
+    Assert.assertEquals("name", m.getName());
+    Assert.assertEquals(Integer.valueOf(1), m.getId());
+  }
+
+  private static final class ComplexTuple extends Tuple2<Tuple2<String, Integer>, Double> {
+    private static final String FORMAT = "ComplexTuple: %s";
+    private final String mToString;
+    public ComplexTuple(final Tuple2<String, Integer> stringIntegerTuple2, final Double aDouble) {
+      super(stringIntegerTuple2, aDouble);
+      mToString = String.format(FORMAT, super.toString());
+    }
+    public Tuple2<String, Integer> getPair() {
+      return mFirst;
+    }
+    public Double getWeight() {
+      return mSecond;
+    }
+    @Override
+    public String toString() {
+      return mToString;
+    }
+  }
+
+  @Test
+  public void testComplexTuple() {
+    final Tuple2<String, Integer> t2 = new MyPair("one", 2);
+    final ComplexTuple c = new ComplexTuple(t2, 3.0);
+    Assert.assertEquals("ComplexTuple: (MyPair: (one, 2), 3.0)", c.toString());
+    Assert.assertEquals(t2, c.getPair());
+    Assert.assertEquals(3.0, c.getWeight());
+  }
+}
